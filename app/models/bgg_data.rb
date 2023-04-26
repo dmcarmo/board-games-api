f2321eded7d540e455c0f13f47b7e9ede76101cb
David Carmo <dmcarmo@gmail.com>
2023-04-26 20:20:53 +0100

    Change class name and methods from class to instance methods

--- app/models/bgg_data.rb
+++ app/models/bgg_data.rb
@@ -1,25 +1,25 @@
 require "faraday"
 require "ox"
 
-class BggData
-  def self.run
+class BggDataImport
+  def run
     # ids_range = (1..last_id)
-    # idea is to split this into batches of 1200 ids
-    # ids_range.each_slice(1200) do |ids|
     ids_range = (1..5)
-    ids_range.each_slice(4) do |ids|
+    # ids_range.each_slice(1200) do |ids|
+    ids_range.each_slice(2) do |ids|
       # each slice/batch should be scheduled into a job
       # each job should be able to handle timeout errors and implement exponential back-off
       # should also have a delay of 1 min (maybe less? some people are using 10 seconds) after the previous one was executed
       url = "#{Game::API_URL}thing?type=boardgame,boardgameexpansion&id=#{ids.join(',')}"
       xml = parse(url)
       parse_data(xml)
+      sleep(30.seconds)
     end
   end
 
-  # private
+  private
 
-  def self.parse_data(xml)
+  def parse_data(xml)
     boardgames = xml.locate("items/item")
     boardgames.each do |boardgame|
       boardgame_data = boardgame_parser(boardgame)
@@ -32,21 +32,21 @@ class BggData
     end
   end
 
-  def self.boardgame_parser(boardgame)
+  def boardgame_parser(boardgame)
     bgg_id = boardgame.attributes[:id]
     name = boardgame.locate("name").find { |line| line.attributes[:type] == "primary" }.attributes[:value]
     year = boardgame.yearpublished.attributes[:value]
     { name: name, bgg_id: bgg_id, yearpublished: year }
   end
 
-  def self.last_id
+  def last_id
     rss_url = "https://boardgamegeek.com/recentadditions/rss?subdomain=&infilters%5B0%5D=thing&infilters%5B1%5D=thinglinked&domain=boardgame"
     recent_additions = parse(rss_url)
     recent_additions.locate("rss/channel/item").map { |item| item.link.text.split("/")[4] }.max
     # recent_additions.locate("rss/channel/item").map { |item| /boardgame\/(\d+)\//.match(item.link.text)[1] }.max
   end
 
-  def self.parse(url)
+  def parse(url)
     response = Faraday.get(url)
     # only parse if response is ok / 200
     Ox.parse(response.body)
