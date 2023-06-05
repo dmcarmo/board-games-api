13d1e3ac0b2a452da9c4768f0c43db42d3d7dc68
David Carmo <dmcarmo@gmail.com>
2023-06-05 01:58:05 +0100

    uncommented URL constants in Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,8 +3,8 @@
 class Game < ApplicationRecord
   has_one_attached :image
 
-  # WEB_URL = "https://boardgamegeek.com/boardgame/"
-  # API_URL = "https://boardgamegeek.com/xmlapi2/"
+  WEB_URL = "https://boardgamegeek.com/boardgame/"
+  API_URL = "https://boardgamegeek.com/xmlapi2/"
 
   include PgSearch::Model
   pg_search_scope :search_by_name,
